ba1fdbc148023ac22c28da4fe81e66d7476148e1
David Adelani <davlanade@gmail.com>
2020-03-14 22:30:07 +0100

    Add files via upload
    bash files for yoruba ner evaluation

---
+++ run_ner_yorubaFM.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+  python BERT_NER_yo.py\
+    --task_name="NER"  \
+    --do_lower_case=True \
+    --crf=True \
+    --do_train=True   \
+    --do_eval=True   \
+    --do_predict=True \
+    --data_dir=ner_data/uncased_nodiacritics_ner_data   \
+    --vocab_file=bert/no_diacritics/vocab.txt  \
+    --bert_config_file=bert/no_diacritics/bert_config.json \
+    --init_checkpoint=bert/no_diacritics/model.ckpt-50000   \
+    --max_seq_length=128   \
+    --train_batch_size=32   \
+    --learning_rate=2e-5   \
+    --num_train_epochs=10.0   \
+    --output_dir=./yoruba_outputFM/result_dir
+
+
+perl conlleval.pl -d '\t' < ./yoruba_outputFM/result_dir/label_test.txt
---
+++ run_ner_yorubaPM.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+  python BERT_NER_yo.py\
+    --task_name="NER"  \
+    --do_lower_case=False \
+    --crf=True \
+    --do_train=True   \
+    --do_eval=True   \
+    --do_predict=True \
+    --data_dir=ner_data/uncased_nodiacritics_ner_data   \
+    --vocab_file=bert/multilingual_L-12_H-768_A-12/vocab.txt  \
+    --bert_config_file=bert/multilingual_L-12_H-768_A-12/bert_config.json \
+    --init_checkpoint=bert/multilingual_L-12_H-768_A-12/bert_model.ckpt   \
+    --max_seq_length=128   \
+    --train_batch_size=32   \
+    --learning_rate=2e-5   \
+    --num_train_epochs=10.0   \
+    --output_dir=./yoruba_outputPM/result_dir
+
+
+perl conlleval.pl -d '\t' < ./yoruba_outputPM/result_dir/label_test.txt
